a1762ff468012cbcf08492c298890cd99dfb374f
codeniu <codeniu@163.com>
2021-03-16 18:54:40 +0800

    feat: add useMount,userDebounce custom hooks

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,34 +1,35 @@
 import * as qs from "qs";
 import { useEffect, useState } from 'react';
-import { cleanObject } from "utils";
+import { cleanObject, useDebounce, useMount } from "utils";
 import { List } from './list';
 import { SearchPanel } from './search-panel';
 const apiUrl = process.env.REACT_APP_API_URL
 
 export const ProjectListScreen = () => { 
-    const [param, setParam] = useState({
-    name: '',
-    personId:''
-    })
+  const [param, setParam] = useState({
+  name: '',
+  personId:''
+  })
+  const debouncedParam = useDebounce(param,2000)
   
   const [list, setList] = useState([])
   const [users, setUsers] = useState([])
 
   useEffect(() => { 
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => { 
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => { 
       if (response.ok) { 
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debouncedParam])
   
-    useEffect(() => {
+    useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
   
   return <div>
     <SearchPanel users={users} param={param} setParam={ setParam}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,10 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
-// 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object) => {
-  // Object.assign({}, object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
-    // 0
     const value = result[key];
     if (isFalsy(value)) {
       delete result[key];
@@ -13,3 +12,32 @@ export const cleanObject = (object) => {
   });
   return result;
 };
+
+// custom hook
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
+};
+
+// export const debounce = (func, delay) => {
+//   let timeout;
+//   return (...param) => {
+//     if (timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(() => {
+//       func(...param);
+//     }, delay);
+//   };
+// };
