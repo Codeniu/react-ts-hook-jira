75d375bcf652d5d67b7d2b6583a11f48767d470e
codeniu <codeniu@163.com>
2021-03-17 20:15:07 +0800

    feat: 登录表单开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { ProjectListScreen } from "screens/project-list";
-import "./App.css";
+import React from 'react';
+import { LoginScreen } from 'screens/login';
+import { ProjectListScreen } from 'screens/project-list';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <ProjectListScreen />
-    </div>
-  );
+    return (
+        <div className="App">
+            <ProjectListScreen />
+            <LoginScreen />
+        </div>
+    );
 }
 
 export default App;
