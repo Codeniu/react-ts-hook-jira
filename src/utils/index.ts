4c9ef0a1f7b8bccf81aa0c517bb37dd1038874cc
codeniu <codeniu@163.com>
2021-03-17 19:23:13 +0800

    chore: 给useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value:any) => (value === 0 ? false : !value);
+export const isFalsy = (value:unknown) => (value === 0 ? false : !value);
 
 export const cleanObject = (object:any) => {
   const result = { ...object };
@@ -21,7 +21,7 @@ export const useMount = (callback:() => void) => {
   }, []);
 };
 
-export const useDebounce = (value:any, delay?:number) => {
+export const useDebounce = <T>(value:T, delay?:number) => {
   const [debounceValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
