d1e6472d04fb71f57513a7d7f4f2ea348c4d5993
codeniu <codeniu@163.com>
2021-03-29 16:14:21 +0800

    fix: 去除警告信息

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
-import 'antd/dist/antd.less';
-import { AppProviders } from 'context';
-import { loadDevTools } from 'jira-dev-tool';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-loadDevTools(() =>
-    ReactDOM.render(
-        <React.StrictMode>
-            <AppProviders>
-                <App />
-            </AppProviders>
-        </React.StrictMode>,
-        document.getElementById('root'),
-    ),
+// 务必在jira-dev-tool后面引入
+import "antd/dist/antd.less";
+import { AppProviders } from "context";
+import { DevTools, loadServer } from "jira-dev-tool";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+loadServer(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <AppProviders>
+        <DevTools />
+        <App />
+      </AppProviders>
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
 
 // If you want to start measuring performance in your app, pass a function
