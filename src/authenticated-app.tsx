dde4913fc4624f61ef7a21041b5da318d306ff57
codeniu <codeniu@163.com>
2021-07-14 11:34:46 +0800

    feat: 添加kanban和epic路由

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -5,9 +5,10 @@ import { ReactComponent as SoftwareLogo } from 'assets/software-logo.svg';
 import styled from '@emotion/styled';
 import { Row } from 'components/lib';
 import { Button, Dropdown, Menu } from 'antd';
-import { Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ProjectScreen } from 'screens/project';
+import { resetRoute } from 'utils';
 
 /**
  * grid 和 flex 各自的应用场景
@@ -22,69 +23,69 @@ import { ProjectScreen } from 'screens/project';
  */
 
 export const AuthenticatedApp = () => {
-    return (
-        <Container>
-            <PageHeader />
-            <Main>
-                <Router>
-                    <Routes>
-                        <Route
-                            path={'/projects'}
-                            element={<ProjectListScreen />}
-                        />
-                        <Route
-                            path={'/projects/:projectId/*'}
-                            element={<ProjectScreen />}
-                        />
-                    </Routes>
-                </Router>
-            </Main>
-        </Container>
-    );
+  return (
+    <Container>
+      <PageHeader />
+      <Main>
+        <Router>
+          <Routes>
+            <Route path={'/projects'} element={<ProjectListScreen />} />
+            <Route
+              path={'/projects/:projectId/*'}
+              element={<ProjectScreen />}
+            />
+            <Navigate to={'/projects'} />
+          </Routes>
+        </Router>
+      </Main>
+    </Container>
+  );
 };
 
 const PageHeader = () => {
-    const { logout, user } = useAuth();
+  const { logout, user } = useAuth();
 
-    return (
-        <Header between={true}>
-            <HeaderLeft gap={true}>
-                <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'} />
-                <h2>项目</h2>
-                <h2>用户</h2>
-            </HeaderLeft>
-            <HeaderRight>
-                <Dropdown
-                    overlay={
-                        <Menu>
-                            <Menu.Item key={'logout'}>
-                                <Button onClick={logout} type={'link'}>
-                                    登出
-                                </Button>
-                            </Menu.Item>
-                        </Menu>
-                    }
-                >
-                    <Button type={'link'} onClick={e => e.preventDefault()}>
-                        Hi, {user?.name}
-                    </Button>
-                </Dropdown>
-            </HeaderRight>
-        </Header>
-    );
+  return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <Button type={'link'} onClick={resetRoute}>
+          <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'} />
+        </Button>
+        <h2>项目</h2>
+        <h2>用户</h2>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key={'logout'}>
+                <Button onClick={logout} type={'link'}>
+                  登出
+                </Button>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button type={'link'} onClick={e => e.preventDefault()}>
+            Hi, {user?.name}
+          </Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
+  );
 };
 
 const Container = styled.div`
-    display: grid;
-    grid-template-rows: 6rem 1fr;
-    height: 100vh;
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
 `;
 
 // grid-area 用来给grid子元素起名字
 const Header = styled(Row)`
-    padding: 3.2rem;
-    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
-    z-index: 1;
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
 `;
 const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 export const isVoid = (value: unknown) =>
-    value === undefined || value === null || value === '';
+  value === undefined || value === null || value === '';
 
 // let a: object
 // a = {name: 'jack'}
@@ -16,23 +16,23 @@ export const isVoid = (value: unknown) =>
 // b = () => {}
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object: { [key: string]: unknown }) => {
-    // Object.assign({}, object)
-    const result = { ...object };
-    Object.keys(result).forEach(key => {
-        const value = result[key];
-        if (isVoid(value)) {
-            delete result[key];
-        }
-    });
-    return result;
+  // Object.assign({}, object)
+  const result = { ...object };
+  Object.keys(result).forEach(key => {
+    const value = result[key];
+    if (isVoid(value)) {
+      delete result[key];
+    }
+  });
+  return result;
 };
 
 export const useMount = (callback: () => void) => {
-    useEffect(() => {
-        callback();
-        // TODO 依赖项里加上callback会造成无限循环，这个和useCallback以及useMemo有关系
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  useEffect(() => {
+    callback();
+    // TODO 依赖项里加上callback会造成无限循环，这个和useCallback以及useMemo有关系
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 };
 
 // const debounce = (func, delay) => {
@@ -63,46 +63,48 @@ export const useMount = (callback: () => void) => {
 
 // 后面用泛型来规范类型
 export const useDebounce = <V>(value: V, delay?: number) => {
-    const [debouncedValue, setDebouncedValue] = useState(value);
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-    useEffect(() => {
-        // 每次在value变化以后，设置一个定时器
-        const timeout = setTimeout(() => setDebouncedValue(value), delay);
-        // 每次在上一个useEffect处理完以后再运行
-        return () => clearTimeout(timeout);
-    }, [value, delay]);
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
 
-    return debouncedValue;
+  return debouncedValue;
 };
 
 export const useArray = <T>(initialArray: T[]) => {
-    const [value, setValue] = useState(initialArray);
-    return {
-        value,
-        setValue,
-        add: (item: T) => setValue([...value, item]),
-        clear: () => setValue([]),
-        removeIndex: (index: number) => {
-            const copy = [...value];
-            copy.splice(index, 1);
-            setValue(copy);
-        },
-    };
+  const [value, setValue] = useState(initialArray);
+  return {
+    value,
+    setValue,
+    add: (item: T) => setValue([...value, item]),
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+  };
 };
 
 export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
-    const oldTitle = document.title;
+  const oldTitle = document.title;
 
-    useEffect(() => {
-        document.title = title;
-    }, [title]);
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
 
-    useEffect(() => {
-        return () => {
-            if (!keepOnUnmount) {
-                document.title = oldTitle;
-            }
-        };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  useEffect(() => {
+    return () => {
+      if (!keepOnUnmount) {
+        document.title = oldTitle;
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 };
+
+export const resetRoute = () => (window.location.href = window.location.origin);
